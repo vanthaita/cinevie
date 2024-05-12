cf2ba8ecd62eeb1d38b507541b71655227b07217
TaThai <thaitv225@gmail.com>
2024-05-12 23:46:30 +0700

    Update #12-5

--- src/app/showtimes/page.js
+++ src/app/showtimes/page.js
@@ -2,25 +2,25 @@ import BoxMovie from "@/components/BoxMovie";
 import movie from "@/constant/movie";
 import DatePicker from "@/components/DatePicker";
 import ShowTimeDropDown from "@/components/ShowtimeDropdown";
+
 export default function ShowTimes() {
     return (
         <section>
-            <div className=" px-20 mt-10 flex flex-col w-full" style={{height: 'calc(370vh - 80px)'}}>
-                <div className=" flex flex-row gap-x-2 justify-center items-center w-full">
-                {/* Drop down */}
-                <ShowTimeDropDown title={"Cinemas"} value={['Aeon Mail', 'Vincom']}/>
-                <DatePicker/>
+            <div className=" md:px-20 px-10 mt-10 mb-20">
+                <div className="flex flex-col md:flex-row gap-2 justify-center items-center w-full">
+                    {/* Drop down */}
+                    <ShowTimeDropDown title={"Cinemas"} value={['Aeon Mail', 'Vincom']} />
+                    <DatePicker />
                 </div>
-                <div className="w-full flex flex-col justify-between items-start mt-10 gap-10">
+                <div className="w-full flex flex-col justify-between items-start mt-10 gap-y-4">
                     {movie.slice(0, 6).map((movie, index) => (
-                        <BoxMovie key={index} 
+                        <BoxMovie key={index}
                             title={movie.Title}
                             genre={movie.Genre}
                             image_url={movie.Poster}
                         />
                     ))}
                 </div>
-
             </div>
         </section>
     )
--- src/components/BoxMovie.js
+++ src/components/BoxMovie.js
@@ -1,57 +1,54 @@
-
-
-export default function BoxMovie({title, genre, image_url}) {
+export default function BoxMovie({ title, genre, image_url }) {
     return (
         <>
-        <div className=" w-full flex flex-col gap-1">
-        <div className=" w-full h-[20rem] flex flex-row justify-between items-center">
-                <div className="w-[185px] rounded-xl h-[250px] justify-center items-center flex">
-                    <img src={image_url} alt="" className=" rounded-2xl w-full h-full"/>
-                </div>
-                <div className="w-[62%] h-[250px] flex flex-col justify-start items-start gap-y-4">
-                    <div className=" flex flex-col justify-start items-start">
-                        <p className=" font-bold text-xl ">{title}</p>
-                        <p className=" font-normal text-sm text-gray-500">{genre}</p>
+            <div className="w-full flex flex-col gap-2 md:gap-1 ">
+                <div className="w-full md:h-[20rem]  flex flex-col md:flex-row justify-between items-center">
+                    <div className="w-[350px] md:w-[185px] h-[450px] md:h-[250px] rounded-xl mb-4 md:mb-0 ">
+                        <img src={image_url} alt="" className="rounded-2xl w-full h-full" />
                     </div>
-                    <div class="flex flex-row justify-between items-start w-full mt-2">
-                        <p class="font-bold text-xl ">Aeon Mail</p>
-                        <div class="grid grid-cols-6 gap-2">
-                            <div class="border rounded-xl  p-4 py-2 flex justify-center items-center cursor-pointer">
-                                <p>09:20 EN</p>
+                    <div className="w-full md:w-[62%] md:h-[250px] mb-10 flex flex-col justify-start items-start md:gap-y-4 gap-y-2">
+                        <div className="flex flex-col justify-start items-start">
+                            <p className="font-bold text-xl">{title}</p>
+                            <p className="font-normal text-sm  text-gray-500">{genre}</p>
+                        </div>
+                        <div className="flex flex-col md:flex-row justify-start items-start w-full mt-2 gap-y-4 gap-x-8">
+                            <p className="font-bold text-xl">Aeon Mail</p>
+                            <div className="grid grid-cols-4 md:grid-cols-6 gap-2">
+                                <div className="border rounded-xl md:p-2 flex justify-center items-center cursor-pointer">
+                                    <p className="text-xs md:text-sm">09:20 EN</p>
+                                </div>
+                                <div className="border rounded-xl p-2 flex justify-center items-center cursor-pointer">
+                                    <p className="text-xs md:text-sm">10:10 EN</p>
+                                </div>
+                                <div className="border rounded-xl p-2 flex justify-center items-center cursor-pointer">
+                                    <p className="text-xs md:text-sm">11:30 EN</p>
+                                </div>
+                                <div className="border rounded-xl p-2 flex justify-center items-center cursor-pointer">
+                                    <p className="text-xs md:text-sm">11:40 EN</p>
+                                </div>
+                                <div className="border rounded-xl p-2 flex justify-center items-center cursor-pointer">
+                                    <p className="text-xs md:text-sm">12:30 EN</p>
+                                </div>
+                                <div className="border rounded-xl p-2 flex justify-center items-center cursor-pointer">
+                                    <p className="text-xs md:text-sm">17:30 EN</p>
+                                </div>
+                                <div className="border rounded-xl p-2 flex justify-center items-center cursor-pointer">
+                                    <p className="text-xs md:text-sm">19:30 EN</p>
+                                </div>
                             </div>
-                            <div class="border rounded-xl  p-4 py-2 flex justify-center items-center cursor-pointer">
-                                <p>10:10 EN</p>
-                            </div>
-                            <div class="border rounded-xl  p-4 py-2 flex justify-center items-center cursor-pointer">
-                                <p>11:30 EN</p>
-                            </div>
-                            <div class="border rounded-xl  p-4 py-2 flex justify-center items-center cursor-pointer">
-                                <p>11:40 EN</p>
-                            </div>
-                            <div class="border rounded-xl  p-4 py-2 flex justify-center items-center cursor-pointer">
-                                <p>12:30 EN</p>
-                            </div>
-                            <div class="border rounded-xl  p-4 py-2 flex justify-center items-center cursor-pointer">
-                                <p>17:30 EN</p>
-                            </div>
-                            <div class="border rounded-xl  p-4 py-2 flex justify-center items-centercursor-pointer">
-                                <p>19:30 EN</p>
+                        </div>
+                        <div className="md:flex flex-row justify-between items-center w-full hidden ">
+                            <div className="p-2 border rounded-lg">
+                                <p className=" font-medium text-xl ">2D</p>
                             </div>
                         </div>
                     </div>
-                    <div className="flex flex-row justify-between items-center w-full">
-                        <div className="p-2 border rounded-lg">
-                            <p className=" font-medium text-xl ">2D</p>
-                        </div>
+                    <div className="w-full md:w-[18%] flex justify-center mb-10">
+                        <button className="btn text-white font-medium px-8 w-[80%] py-2 bg-gradient-to-b from-orange-300 to-pink-400 hover:to-pink-300 border-none rounded-full">Book a session</button>
                     </div>
                 </div>
-                <div className="h-[250px] w-[18%] flex justify-end items-center mb-[3.7rem]">
-                    <button className="btn text-white font-medium  px-12 bg-gradient-to-b from-orange-300 to-pink-400 hover:to-pink-300 border-none rounded-full ">Book a session</button>
-                </div>
+                <div className="border-b-2 border-gray-500 w-full" />
             </div>
-            </div>   
-            <div className="border-b-2 border-gray-500 w-full" />
         </>
-        
-    )
+    );
 }
